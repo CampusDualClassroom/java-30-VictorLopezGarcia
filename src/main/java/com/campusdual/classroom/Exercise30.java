1a0e3f9aa8bb78d87b1b12c90a2fdfd9add951da
VictorLopezGarcia <victor.lopezg@udc.es>
2025-05-23 09:34:48 +0200

    Ejercicio30

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -2,11 +2,23 @@ package com.campusdual.classroom;
 
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
 
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+        if (divisor == 0) {
+            throw new DivisionByZeroException();
+        }
+        return dividend / divisor;
     }
 
     public static void main(String[] args) {
-
+        int result = 0;
+        try{
+            result=divisionWithCustomException(20, 0);
+        }catch (DivisionByZeroException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Result: " + result);
     }
+
+
 }
